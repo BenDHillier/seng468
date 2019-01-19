dd9fd533bd9a54c2c5989cbe2a48b5672e06c5d4
Cameron Elwood <celwood93@gmail.com>
2019-01-19 10:20:08 -0800

    blindly added everything

---
+++ StockTrader/src/main/java/com/restResource/StockTrader/StockTraderApplication.java
@@ -0,0 +1,14 @@
+package com.restResource.StockTrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTraderApplication.class, args);
+	}
+
+}
+
