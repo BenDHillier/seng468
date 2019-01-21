9c3956fb9129df1a5d0a18e39802b067e4b3e9f0
BenDHillier <ben@divdot.com>
2019-01-20 23:48:08 -0800

    add insertOrIncrement query to investmentRepository

--- StockTrader/src/main/java/com/restResource/StockTrader/repository/InvestmentRepository.java
+++ StockTrader/src/main/java/com/restResource/StockTrader/repository/InvestmentRepository.java
@@ -2,7 +2,18 @@ package com.restResource.StockTrader.repository;
 
 import com.restResource.StockTrader.entity.Investment;
 import com.restResource.StockTrader.entity.InvestmentId;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface InvestmentRepository extends CrudRepository<Investment, InvestmentId> {
+    @Modifying
+    @Transactional
+    @Query(value =
+            "INSERT INTO investment VALUES (?1, ?2, ?3) " +
+            "ON CONFLICT (owner, stock_symbol) DO UPDATE " +
+            "SET amount = investment.amount + ?3",
+            nativeQuery = true)
+    void insertOrIncrement(String owner, String stockSymbol, Integer amount);
 }
