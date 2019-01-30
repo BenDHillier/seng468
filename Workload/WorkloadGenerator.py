8b8eb936799de1303cce5e2f9d2ff04baaf39bb2
AlexDeweert <alexander.deweert@gmail.com>
2019-01-29 20:09:36 -0800

    pushing workload generator to master

--- Workload/WorkloadGenerator.py
+++ Workload/WorkloadGenerator.py
@@ -1,7 +1,14 @@
 import httplib
 import urllib
+import urllib2
 import sys
 import time
+
+# TODO: SET_BUY_AMOUNT, CANCEL_SET_BUY, SET_BUY_TRIGGER, SET_SELL_AMOUNT, CANCEL_SET_SELL, SET_SELL_TRIGGER
+# Will do the above functions once triggers are in place
+
+# TODO: DISPLAY_SUMMARY
+# Will do DISPLAY_SUMMARY once logging is in place
 
 class WorkloadGenerator:
     def __init__(self, *args):
@@ -29,18 +36,29 @@ class WorkloadGenerator:
             params = split_line[1:]
             print("sending: " + command + " with params {}".format(params))
             self.handleCommand(command,params)
-            #time.sleep(.5)
+            #time.sleep(.250)
 
     def handleCommand(self,cmd,params):
         if( cmd == "ADD"):
             self.addRequest(params)
         elif( cmd == "BUY"):
             self.buyRequest(params)
+        elif( cmd == "COMMIT_BUY"):
+            self.commitBuyRequest(params)
+        elif( cmd == "CANCEL_BUY"):
+            self.cancelBuyRequest(params)
         elif( cmd == "QUOTE"):
-            print("command was quote")
+            self.quoteRequest(params)
+        elif( cmd == "SELL"):
+            self.sellRequest(params)
+        elif( cmd == "COMMIT_SELL"):
+            self.commitSellRequest(params)
+        elif( cmd == "CANCEL_SELL"):
+            self.cancelSellRequest(params)
         else:
             print("command was " + cmd)
 
+    #PUT
     def addRequest(self,params):
         try:
             # TODO: Eventually will have to send unformatted string of float val
@@ -57,6 +75,17 @@ class WorkloadGenerator:
         except Exception as e:
             print "ADD,{},{} failed due to exception {}".format(params[0],params[1],e)
 
+    #GET
+    def quoteRequest(self,params):
+        try:
+            get_params = urllib.urlencode({'userId':params[0],'stockSymbol':params[1]})
+            get_request = urllib2.urlopen('http://{}:{}/quote?'.format(self._args_ip,self._args_port) + get_params)
+            response = get_request.read()
+            print(response)
+        except Exception as e:
+            print "QUOTE,{},{} failed due to exception {}".format(params[0],params[1],e)
+
+    #POST
     def buyRequest(self,params):
         try:
             post_params = urllib.urlencode({'userId':params[0],'stockSymbol':params[1],'amount':int(float(params[2]))})
@@ -69,7 +98,84 @@ class WorkloadGenerator:
             else:
                 print(response.status)
         except Exception as e:
-            print "BUY,{},{} failed due to exception {}".format(params[0],params[1],e)
+            print "BUY,{},{},{} failed due to exception {}".format(params[0],params[1],params[2],e)
+
+    #POST
+    # TODO: confirm this is the /create route in server
+    def commitBuyRequest(self,params):
+        try:
+            post_params = urllib.urlencode({'userId':params[0]})
+            headers = {"Content-type": "application/x-www-form-urlencoded"}
+            self._httpconnection.request('POST', '/buy/commit', post_params, headers)
+            response = self._httpconnection.getresponse()
+            data = response.read()
+            if( response.status == 200 ):
+                print("Response from server: " + data)
+            else:
+                print(response.status)
+        except Exception as e:
+            print "COMMIT_BUY,{} failed due to exception {}".format(params[0],e)
+
+    #POST
+    def cancelBuyRequest(self,params):
+        try:
+            post_params = urllib.urlencode({'userId':params[0]})
+            headers = {"Content-type": "application/x-www-form-urlencoded"}
+            self._httpconnection.request('POST', '/buy/cancel', post_params, headers)
+            response = self._httpconnection.getresponse()
+            data = response.read()
+            if( response.status == 200 ):
+                print("Response from server: " + data)
+            else:
+                print(response.status)
+        except Exception as e:
+            print "CANCEL_BUY,{} failed due to exception {}".format(params[0],e)
+
+    #POST
+    def sellRequest(self,params):
+        try:
+            post_params = urllib.urlencode({'userId':params[0],'stockSymbol':params[1],'amount':int(float(params[2]))})
+            headers = {"Content-type": "application/x-www-form-urlencoded"}
+            self._httpconnection.request('POST', '/sell/create', post_params, headers)
+            response = self._httpconnection.getresponse()
+            data = response.read()
+            if( response.status == 200 ):
+                print("Response from server: " + data)
+            else:
+                print(response.status)
+        except Exception as e:
+            print "SELL,{},{} failed due to exception {}".format(params[0],params[1],e)
+
+    #POST
+    def commitSellRequest(self,params):
+        try:
+            post_params = urllib.urlencode({'userId':params[0]})
+            headers = {"Content-type": "application/x-www-form-urlencoded"}
+            self._httpconnection.request('POST', '/sell/commit', post_params, headers)
+            response = self._httpconnection.getresponse()
+            data = response.read()
+            if( response.status == 200 ):
+                print("Response from server: " + data)
+            else:
+                print(response.status)
+        except Exception as e:
+            print "COMMIT_SELL,{} failed due to exception {}".format(params[0],e)
+
+    #POST
+    def cancelSellRequest(self,params):
+        try:
+            post_params = urllib.urlencode({'userId':params[0]})
+            headers = {"Content-type": "application/x-www-form-urlencoded"}
+            self._httpconnection.request('POST', '/sell/cancel', post_params, headers)
+            response = self._httpconnection.getresponse()
+            data = response.read()
+            if( response.status == 200 ):
+                print("Response from server: " + data)
+            else:
+                print(response.status)
+        except Exception as e:
+            print "CANCEL_SELL,{} failed due to exception {}".format(params[0],e)
+
 
 if __name__ == "__main__":
     client = WorkloadGenerator(sys.argv)
