32afe4ed6d2924a61958f3ce0a9574c4f1ea8238
BenDHillier <ben@divdot.com>
2019-01-31 22:02:28 -0800

    add quote log

--- StockTrader/src/main/java/com/restResource/StockTrader/controller/QuoteController.java
+++ StockTrader/src/main/java/com/restResource/StockTrader/controller/QuoteController.java
@@ -1,6 +1,9 @@
 package com.restResource.StockTrader.controller;
 
+import com.restResource.StockTrader.entity.CommandType;
 import com.restResource.StockTrader.entity.Quote;
+import com.restResource.StockTrader.entity.logging.UserCommandLog;
+import com.restResource.StockTrader.service.LoggingService;
 import com.restResource.StockTrader.service.QuoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuoteController {
     private QuoteService quoteService;
 
+    private LoggingService loggingService;
+
     public QuoteController(
-            QuoteService quoteService) {
+            QuoteService quoteService,
+            LoggingService loggingService) {
 
         this.quoteService = quoteService;
+        this.loggingService = loggingService;
     }
     //does not account for caching -> needs to be updated
     @GetMapping(value = "/quote")
     public
     ResponseEntity<Quote> getQuotePrice(@RequestParam String stockSymbol,
                                  @RequestParam String userId) {
+
+        loggingService.logUserCommand(
+                UserCommandLog.builder()
+                        .stockSymbol(stockSymbol)
+                        .username(userId)
+                        .command(CommandType.QUOTE)
+                        .build());
+
         try {
             Quote quote = quoteService.getQuote(stockSymbol, userId);
             return new ResponseEntity<>(quote, HttpStatus.OK);
