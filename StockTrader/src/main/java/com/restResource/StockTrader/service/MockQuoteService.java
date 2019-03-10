4645bea1efc928a310e22bc43fa0dfc5c6d84623
Cameron Elwood <celwood93@gmail.com>
2019-03-10 14:26:23 -0700

    Integrated redis cache into our quote service.

--- StockTrader/src/main/java/com/restResource/StockTrader/service/MockQuoteService.java
+++ StockTrader/src/main/java/com/restResource/StockTrader/service/MockQuoteService.java
@@ -1,12 +1,9 @@
 package com.restResource.StockTrader.service;
 
-import com.restResource.StockTrader.entity.CommandType;
 import com.restResource.StockTrader.entity.Quote;
 import com.restResource.StockTrader.entity.logging.QuoteServerLog;
-import com.restResource.StockTrader.entity.logging.SystemEventLog;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-import redis.clients.jedis.Jedis;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -15,14 +12,11 @@ import java.util.Optional;
 @Profile("!prod")
 public class MockQuoteService implements QuoteService {
     private LoggingService loggingService;
-    private Jedis jedis;
-    private MockQuoteService(LoggingService loggingService, Jedis jedis) {
+    private MockQuoteService(LoggingService loggingService) {
         this.loggingService = loggingService;
-        this.jedis = jedis;
     }
     @Override
     public Optional<Quote> getQuote(String stockSymbol, String userId, int transactionNum) {
-        jedis.hset("user#1", "name", "Peter");
         loggingService.logQuoteServer(
                 QuoteServerLog.builder()
                         .price("50")
