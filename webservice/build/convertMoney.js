3a275ed1696185ff699bbf2838bc34cb0883478b
AlexDeweert <alexander.deweert@gmail.com>
2019-04-05 14:38:03 -0700

    added support for lonely numbers

--- webservice/build/convertMoney.js
+++ webservice/build/convertMoney.js
@@ -3,6 +3,10 @@ const config = require('./config.json');
 module.exports = {
     convertNumber: function(number){
         if (/^\d+(\.\d\d)?$/.test(number)) {
+            if(/^\d+$/.test(number)){
+                number = number*100;
+                return number;
+            }
             number = number.replace("\.", "");
             return number
         }else{
